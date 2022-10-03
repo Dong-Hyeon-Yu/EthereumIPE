25fc957210ce1d8e3fab78bbd1ce686d38c21856
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-10-04 00:40:30 +0900

    add benchmarking rizen7 2700x

--- benchmarking/do_benchmark.sh
+++ benchmarking/do_benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-path=~/ydh_workspace/EthereumIPE/
+path=$(pwd)/..
 cd $path/Geth/core/vm
 go test -bench BenchmarkPrecompiled -benchtime 5s -timeout 30m 1> $path/benchmarking/rawdata/geth-$(date +'%y%m%d-%H%M%S')-ydh.txt 2>&1
 echo "benchmarking is done."
